f7d67984d2a32619176016367954caf9c880a6c1
Tilen Majerle <tilen.majerle@gmail.com>
2020-04-09 14:58:39 +0200

    Create file.c

---
+++ file.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int
+main {
+    printf("User name 1: ");
+    printf("User\r\n");
+    printf("User name 2: ");
+    printf("User\r\n");
+    printf("User name 3: ");
+    printf("User\r\n");
+}
